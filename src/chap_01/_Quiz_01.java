d2def06e93c715a398da13068cf921ed208f6beb
ãmuseundal <ehdus1105@gmail.com>
2023-10-18 17:55:39 +0900

    [:hammer:]나도코딩 챕터 1 끝

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+
+        String busNo = "신촌08";
+        int minute = 10;
+        double distance = 7.7;
+
+        System.out.println(busNo+"번 버스");
+        System.out.println("약 "+minute+"분 후 도착");
+        System.out.println("남은 거리"+distance+"km");
+    }
+}
